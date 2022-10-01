94105cc5abf8c69603fa9200a8945bddb9483e2f
kaushik karan singh <kaushikkaransinghkks@gmail.com>
2022-10-01 14:55:18 +0530

    Added 3 three programs

---
+++ Basic Python Pratice Program/1_Odd_Even.py
@@ -0,0 +1,9 @@
+
+
+#Program for to check given no is odd or even !!! By kks
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
---
+++ Basic Python Pratice Program/2_ArthmeticOpertor.py
@@ -0,0 +1,20 @@
+
+
+#Arthmetic operaror by KKS
+
+
+val1 = 4
+val2 = 2
+  
+
+sum = val1 + val2
+diff = val1 - val2
+multi = val1 * val2
+div = val1 / val2
+mod = val1 % val2
+
+print(sum)
+print(diff)
+print(multi)
+print(div)
+print(mod)
